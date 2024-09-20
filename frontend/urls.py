f9b42e85b8dd70f692b5fc58a6bcdef1e130b5f0
tonypal-2002 <tonypal2002@gmail.com>
2024-09-20 15:48:51 +0530

    static asset frontend linked

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from frontend.views import forget_password
+from frontend.views import forget_password, home
 
 urlpatterns = [
+    path('', home),
+    path('/', home, name='home'),
     path('forget_password', forget_password),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'frontend/home.html')
+
 def forget_password(request):
     return render(request, 'frontend/forget_password.html')
 # Create your views here.
