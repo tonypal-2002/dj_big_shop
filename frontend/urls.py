455caffc7ecd058eec5aa633700ea2e09b2844d9
tonypal-2002 <tonypal2002@gmail.com>
2024-09-20 11:33:44 +0530

    Partial Setup

---
+++ frontend/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from frontend.views import forget_password
+
+urlpatterns = [
+    path('forget_password', forget_password),
+]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def forget_password(request):
+    return render(request, 'frontend/forget_password.html')
 # Create your views here.
