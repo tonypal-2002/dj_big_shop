72b57206594e38fb9ac9080e3f34b133b021a843
tonypal-2002 <tonypal2002@gmail.com>
2024-09-21 13:07:20 +0530

    preloader added

--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'frontend/home.html')
 
-def forget_password(request):
-    return render(request, 'frontend/forget_password.html')
+
+
 # Create your views here.
