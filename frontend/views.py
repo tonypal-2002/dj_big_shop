c00e3404ee229ba521a28f28e6cf24f32620d51e
tonypal-2002 <tonypal2002@gmail.com>
2024-09-22 21:51:29 +0530

    frontend -> register.html added

--- frontend/views.py
+++ frontend/views.py
@@ -6,4 +6,7 @@ def home(request):
 def login(request):
     return render(request, 'frontend/auth/login.html')
 
+def register(request):
+    return render(request, 'frontend/auth/register.html')
+
 # Create your views here.
