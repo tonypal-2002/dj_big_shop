0e09283b8de0a4ddc7edfa06be1688b41bbca9e1
tonypal-2002 <tonypal2002@gmail.com>
2024-09-24 12:47:21 +0530

    product list type1 listing page added

--- frontend/views.py
+++ frontend/views.py
@@ -8,9 +8,25 @@ def home(request):
 
     categories = Category.objects.all()
 
+    # Check if 'category' query parameter is present
+    category_present = 'category' in request.GET
+
     data = {
         'categories': categories,
+        'category_present': category_present,
     }
+
+    # Apply category filter if category is present and is not 'All'
+    category_id = request.GET.get('category')
+    if category_id and category_id != 'All':
+
+        # Filter products based on the selected category
+        products = Product.objects.filter(category__id=category_id)
+
+        # Pass filtered products to the template
+        data['products'] = products
+
+        return render(request, 'frontend/product/list/type1.html', data)
     return render(request, 'frontend/home.html', data)
 
 def login(request):
