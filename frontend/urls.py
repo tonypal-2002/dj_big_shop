dbbce51d25e017e30478aa6f80ae79226d75c50c
tonypal-2002 <tonypal2002@gmail.com>
2024-10-05 16:01:07 +0530

    Logout Logic and UI added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from frontend.views import home, auth_login, register, forget_password, about, account, show
+from frontend.views import home, auth_login,auth_logout, register, forget_password, about, account, show
 
 urlpatterns = [
     path('', home, name='home'),
     path('product/<int:product_id>/', show, name='show'),  # Product detail page
     #path('/', home, name='home'),
     path('login', auth_login, name='login'),
+    path('logout/', auth_logout, name='logout'),
     path('register', register, name='register'),
     path('forget_password', forget_password, name='forget_password'),
     path('account', account, name='account'),
